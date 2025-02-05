b0cd9c13994d15a7cf892c8aa2ee6bc06c0e2e1b
Arulkumara Babu <22z310@psgtech.ac.in>
2025-02-05 23:17:50 +0530

    privacy

--- src/pages/sections/Section9.js
+++ src/pages/sections/Section9.js
@@ -3,7 +3,7 @@ import { BsInstagram, BsLinkedin, BsTelephone } from "react-icons/bs";
 import { SiLinktree } from "react-icons/si";
 import { TbBrandLinktree } from "react-icons/tb";
 import { SiGmail } from "react-icons/si";
-
+import { AiOutlineFileText } from "react-icons/ai";
 // Contact Us
 
 const Section9 = () => {
@@ -17,7 +17,6 @@ const Section9 = () => {
         <h1 className="text-5xl text-white font-bold pb-6">Contact Us</h1>
         <div className="flex flex-col lg:flex-row gap-8 flex-wrap text-white mt-8">
           <div className="space-y-6 lg:w-56 lg:mr-12">
-           
             <button
               className="flex items-center space-x-4"
               onClick={() =>
@@ -33,20 +32,37 @@ const Section9 = () => {
             </button>
             <button
               className="flex items-center space-x-4"
-              onClick={() => window.open("mailto:cea.civil@psgtech.ac.in", "_blank")}
+              onClick={() =>
+                window.open("mailto:cea.civil@psgtech.ac.in", "_blank")
+              }
             >
               <SiGmail size={24} className="text-white" />
               <p className="text-lg"></p>
               Gmail
             </button>
-            
+            <button
+              className="flex items-center space-x-4"
+              onClick={() => (window.location.href = "/privacypolicy.html")}
+            >
+              <AiOutlineFileText size={24} className="text-white" />
+              <p className="text-lg">Privacy Policy</p>
+            </button>
+
             {}
           </div>
           <div className="flex-1 flex flex-col lg:flex-wrap gap-8 lg:gap-6 lg:gap-y-12 lg:flex-row items-center ">
             <div className="w-full lg:w-56">
               <h5 className="text-xs tracking-widest">Secretaries</h5>
-              <ContactItem name="Ellakiyaa A" phone="+91 7598436113 " isPresent={true} />
-              <ContactItem name="Kaveiya Priya S" phone="+91 9345773847" isPresent={true} />
+              <ContactItem
+                name="Ellakiyaa A"
+                phone="+91 7598436113 "
+                isPresent={true}
+              />
+              <ContactItem
+                name="Kaveiya Priya S"
+                phone="+91 9345773847"
+                isPresent={true}
+              />
             </div>
             <div className="w-full lg:w-56 whitespace-nowrap">
               <h5 className="text-xs tracking-widest">Developers</h5>
@@ -57,13 +73,7 @@ const Section9 = () => {
               <ContactItem name="Mithilesh EN" />
               <ContactItem name="Navaneetha Krishnan K S" />
               <ContactItem name="Kishoreadhith" />
-            
             </div>
-            
-            
-            
-
-
           </div>
         </div>
       </section>
@@ -71,21 +81,23 @@ const Section9 = () => {
   );
 };
 
-const ContactItem = ({ name, phone, className = ""  , isPresent}) => {
+const ContactItem = ({ name, phone, className = "", isPresent }) => {
   return (
     <div className={`${className}`}>
       <h4 className="text-lg mt-2 font-semibold">{name}</h4>
-      {isPresent ? (<>
-        <button
-        className="flex items-center space-x-4 mt-2"
-        onClick={() => window.open(`tel:${phone}`)}
-      >
-     
-        <BsTelephone className="text-white" />
-        <p className="text-sm">{phone}</p>
-      </button>
-      </>) : <></>}
-     
+      {isPresent ? (
+        <>
+          <button
+            className="flex items-center space-x-4 mt-2"
+            onClick={() => window.open(`tel:${phone}`)}
+          >
+            <BsTelephone className="text-white" />
+            <p className="text-sm">{phone}</p>
+          </button>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
